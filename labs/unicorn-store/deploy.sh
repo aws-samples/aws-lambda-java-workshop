1be246fc37f3e48b90aa6797cd667099b82289e0
Maximilian Schellhorn <mxschell@amazon.de>
2022-07-05 12:14:32 +0200

    Added Cloud 9 Setup files

--- labs/unicorn-store/deploy.sh
+++ labs/unicorn-store/deploy.sh
@@ -6,7 +6,7 @@ if [ $app == "spring" ]
 then
   ./mvnw clean package -f software/unicorn-store-spring/pom.xml
   cd infrastructure/cdk
-  cdk deploy UnicornStoreSpringApp --outputs-file target/output.json
+  cdk deploy UnicornStoreSpringApp --outputs-file target/output.json --require-approval never
   exit 0
 fi
 
@@ -14,7 +14,7 @@ if [ $app == "micronaut" ]
 then
   ./mvnw clean package -f software/unicorn-store-micronaut/pom.xml
   cd infrastructure/cdk
-  cdk deploy UnicornStoreSpringApp --outputs-file target/output.json
+  cdk deploy UnicornStoreSpringApp --outputs-file target/output.json --require-approval never
   exit 0
 fi
 
@@ -22,15 +22,7 @@ if [ $app == "basic" ]
 then
   ./mvnw clean package -f software/unicorn-store-basic/pom.xml
   cd infrastructure/cdk
-  cdk deploy UnicornStoreSpringApp --outputs-file target/output.json
-  exit 0
-fi
-
-if [ $app == "spring-security" ]
-then
-  ./mvnw clean package -f software/unicorn-store-spring-security/pom.xml
-  cd infrastructure/cdk
-  cdk deploy UnicornStoreSpringSecurityApp --outputs-file target/output.json
+  cdk deploy UnicornStoreSpringApp --outputs-file target/output.json --require-approval never
   exit 0
 fi
 
