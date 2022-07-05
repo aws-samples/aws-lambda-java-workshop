dee2a5c264227060446c024a6bbe243294123686
Maximilian Schellhorn <mxschell@amazon.de>
2022-07-05 14:44:02 +0200

    Added Cloud 9 Setup files

--- setup-cloud9.sh
+++ setup-cloud9.sh
@@ -1,3 +1,8 @@
+## Install Maven
+sudo wget http://repos.fedorapeople.org/repos/dchen/apache-maven/epel-apache-maven.repo -O /etc/yum.repos.d/epel-apache-maven.repo
+sudo sed -i s/\$releasever/6/g /etc/yum.repos.d/epel-apache-maven.repo
+sudo yum install -y apache-maven
+
 # Install newer version of AWS SAM CLI
 wget https://github.com/aws/aws-sam-cli/releases/download/v1.53.0/aws-sam-cli-linux-x86_64.zip
 unzip -q aws-sam-cli-linux-x86_64.zip -d sam-installation
