8bc85aca2cba93b73413853f9e93057866a7549e
Dennis Kieselhorst <mail@dekies.de>
2022-07-19 14:20:04 +0200

    polished setup-cloud9.sh

--- setup-cloud9.sh
+++ setup-cloud9.sh
@@ -4,8 +4,9 @@ cd /tmp
 ## Ensure AWS CLI v2 is installed
 sudo yum -y remove aws-cli
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-unzip awscliv2.zip
+unzip -q awscliv2.zip
 sudo ./aws/install
+rm awscliv2.zip
 
 ## Install Maven
 export MVN_VERSION=3.8.6
@@ -13,9 +14,10 @@ export MVN_FOLDERNAME=apache-maven-${MVN_VERSION}
 export MVN_FILENAME=apache-maven-${MVN_VERSION}-bin.tar.gz
 curl -4 -L https://dlcdn.apache.org/maven/maven-3/${MVN_VERSION}/binaries/${MVN_FILENAME} | tar -xvz
 sudo mv $MVN_FOLDERNAME /usr/lib/maven
-rm -rf $MVN_FOLDERNAME
+rm $MVN_FILENAME
 export M2_HOME=/usr/lib/maven
 export PATH=${PATH}:${M2_HOME}/bin
+sudo ln -s /usr/lib/maven/bin/mvn /usr/local/bin 
 
 # Install newer version of AWS SAM CLI
 wget -q https://github.com/aws/aws-sam-cli/releases/latest/download/aws-sam-cli-linux-x86_64.zip
