70561dcc0d2cc5c64012408754fd8b5ca4068c93
Dennis Kieselhorst <mail@dekies.de>
2022-07-19 14:43:41 +0200

    polished setup-cloud9.sh

--- setup-cloud9.sh
+++ setup-cloud9.sh
@@ -14,7 +14,6 @@ export MVN_FOLDERNAME=apache-maven-${MVN_VERSION}
 export MVN_FILENAME=apache-maven-${MVN_VERSION}-bin.tar.gz
 curl -4 -L https://dlcdn.apache.org/maven/maven-3/${MVN_VERSION}/binaries/${MVN_FILENAME} | tar -xvz
 sudo mv $MVN_FOLDERNAME /usr/lib/maven
-rm $MVN_FILENAME
 export M2_HOME=/usr/lib/maven
 export PATH=${PATH}:${M2_HOME}/bin
 sudo ln -s /usr/lib/maven/bin/mvn /usr/local/bin 
