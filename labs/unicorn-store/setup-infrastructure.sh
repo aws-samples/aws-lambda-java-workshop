cc4ff092f1f5c540a3c972a2f67999682d34e54c
Maximilian Schellhorn <mxschell@amazon.de>
2022-09-30 14:20:27 +0200

    Adapted build

--- labs/unicorn-store/setup-infrastructure.sh
+++ labs/unicorn-store/setup-infrastructure.sh
@@ -4,9 +4,9 @@
 ./mvnw clean package -f infrastructure/db-setup/pom.xml
 
 # Build the unicorn application
-./mvnw clean package -f software/unicorn-store-basic/pom.xml
-./mvnw clean package -f software/unicorn-store-spring/pom.xml
-./mvnw clean package -f software/unicorn-store-micronaut/pom.xml
+./mvnw clean package -f software/alternatives/unicorn-store-basic/pom.xml
+./mvnw clean package -f software/alternatives/unicorn-store-spring/pom.xml
+./mvnw clean package -f software/alternatives/unicorn-store-micronaut/pom.xml
 
 # Deploy the infrastructure
 cd infrastructure/cdk
