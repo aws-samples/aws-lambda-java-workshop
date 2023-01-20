0e8149555ba8a98b8a1f70ee8f8455590b3bfe49
Maximilian Schellhorn <mxschell@amazon.de>
2023-01-20 13:09:13 +0100

    Added comments

--- labs/unicorn-location-api/graal/build-native-image.sh
+++ labs/unicorn-location-api/graal/build-native-image.sh
@@ -6,7 +6,9 @@ docker build . -t graalvm-lambda-builder --progress=plain
 #Extract the resulting native image
 docker run --rm --entrypoint cat graalvm-lambda-builder target/native > UnicornLocationFunctionGraalVM/native
 
+#Provide proper permissions to execute
 chmod +x UnicornLocationFunctionGraalVM/native
 chmod +x UnicornLocationFunctionGraalVM/bootstrap
 
+#Zip the files
 zip UnicornLocationFunctionGraalVM/uncicorn-location-graal.zip UnicornLocationFunctionGraalVM/native UnicornLocationFunctionGraalVM/bootstrap
