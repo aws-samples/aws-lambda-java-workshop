1f113b5487de7cb73beecc88c651a5f843650936
Maximilian Schellhorn <mxschell@amazon.de>
2023-01-20 13:48:37 +0100

    Adapted native image packaging

--- labs/unicorn-location-api/graal/build-native-image.sh
+++ labs/unicorn-location-api/graal/build-native-image.sh
@@ -3,12 +3,5 @@
 #Build the native image via Docker
 docker build . -t graalvm-lambda-builder --progress=plain
 
-#Extract the resulting native image
-docker run --rm --entrypoint cat graalvm-lambda-builder target/native > UnicornLocationFunctionGraalVM/native
-
-#Provide proper permissions to execute
-chmod +x UnicornLocationFunctionGraalVM/native
-chmod +x UnicornLocationFunctionGraalVM/bootstrap
-
-#Zip the files
-zip UnicornLocationFunctionGraalVM/uncicorn-location-graal.zip UnicornLocationFunctionGraalVM/native UnicornLocationFunctionGraalVM/bootstrap
+#Extract the resulting zip file with the native image and starting instructions
+docker run --rm --entrypoint cat graalvm-lambda-builder unicorn-location-graal.zip > UnicornLocationFunctionGraalVM/unicorn-location-graal.zip
