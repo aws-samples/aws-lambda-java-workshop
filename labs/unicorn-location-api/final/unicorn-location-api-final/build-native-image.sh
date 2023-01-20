3b71dceaa43e52f69572efef1f28288a707d932e
maschnetwork <36627945+maschnetwork@users.noreply.github.com>
2023-01-20 16:25:25 +0100

    Graalvm packaging (#339)
    * Adapted GraalVM Sample

--- labs/unicorn-location-api/final/unicorn-location-api-final/build-native-image.sh
+++ labs/unicorn-location-api/final/unicorn-location-api-final/build-native-image.sh
@@ -3,5 +3,5 @@
 #Build the native image via Docker
 docker build . -t graalvm-lambda-builder --progress=plain
 
-#Extract the resulting native image
-docker run --rm --entrypoint cat graalvm-lambda-builder target/native > UnicornLocationFunctionGraalVM/native
+#Extract the resulting zip file with the native image and starting instructions
+docker run --rm --entrypoint cat graalvm-lambda-builder unicorn-location-graal.zip > UnicornLocationFunctionGraalVM/unicorn-location-graal.zip
