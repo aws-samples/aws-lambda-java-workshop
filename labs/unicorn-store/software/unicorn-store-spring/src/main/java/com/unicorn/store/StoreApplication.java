3c22c2850d9d3e383d46e2abb86341778fc07d3c
Dennis Kieselhorst <mail@dekies.de>
2023-08-18 15:47:46 +0200

    remove customer handlerMapping and handlerAdapter (causes curl request to fail with org.springframework.web.HttpMediaTypeNotSupportedException: Content-Type 'application/json;charset=UTF-8' is not supported])

--- labs/unicorn-store/software/unicorn-store-spring/src/main/java/com/unicorn/store/StoreApplication.java
+++ labs/unicorn-store/software/unicorn-store-spring/src/main/java/com/unicorn/store/StoreApplication.java
@@ -9,10 +9,6 @@ import com.unicorn.store.controller.UnicornController;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
-import org.springframework.web.servlet.HandlerAdapter;
-import org.springframework.web.servlet.HandlerMapping;
-import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter;
-import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
 
 
 @SpringBootApplication
@@ -29,18 +25,5 @@ public class StoreApplication {
 		objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
 		return objectMapper;
 	}
-	
-	@Bean
-    public HandlerMapping handlerMapping() {
-        return new RequestMappingHandlerMapping();
-    }
-
-    /*
-     * Create required HandlerAdapter, to avoid several default HandlerAdapter instances being created
-     */
-    @Bean
-    public HandlerAdapter handlerAdapter() {
-        return new RequestMappingHandlerAdapter();
-    }
 
 }
