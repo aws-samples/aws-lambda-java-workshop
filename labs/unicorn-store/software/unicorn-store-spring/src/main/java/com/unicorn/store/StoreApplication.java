96245ee9dea3908d38dbdda513377da113acf1a5
Dennis Kieselhorst <mail@dekies.de>
2023-08-21 15:54:26 +0200

    removed unnecessary @Import

--- labs/unicorn-store/software/unicorn-store-spring/src/main/java/com/unicorn/store/StoreApplication.java
+++ labs/unicorn-store/software/unicorn-store-spring/src/main/java/com/unicorn/store/StoreApplication.java
@@ -3,16 +3,12 @@ package com.unicorn.store;
 import org.springframework.boot.SpringApplication;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.unicorn.store.controller.UnicornController;
-
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Import;
 
 
 @SpringBootApplication
-@Import({UnicornController.class})
 public class StoreApplication {
 
 	public static void main(String[] args) {
