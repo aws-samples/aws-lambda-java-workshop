fb77ecd587e13511148f6ba0e7f4a5628724443f
Dennis Kieselhorst <mail@dekies.de>
2023-08-22 13:43:28 +0200

    use Spring Boot 3 instead of experimental Spring Native capabilities

--- labs/unicorn-store/software/alternatives/unicorn-store-spring-native-graalvm/src/main/java/com/unicorn/store/StoreApplication.java
+++ labs/unicorn-store/software/alternatives/unicorn-store-spring-native-graalvm/src/main/java/com/unicorn/store/StoreApplication.java
@@ -1,29 +1,15 @@
 package com.unicorn.store;
 
-import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.unicorn.store.model.Unicorn;
-import com.unicorn.store.model.UnicornEventType;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jdbc.repository.config.EnableJdbcRepositories;
-import org.springframework.nativex.hint.TypeAccess;
-import org.springframework.nativex.hint.TypeHint;
 
 @SpringBootApplication
 @EnableJdbcRepositories
-@TypeHint(types = {
-		APIGatewayProxyRequestEvent.class
-},
-		typeNames = {
-				"com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent$ProxyRequestContext",
-				"com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent$RequestIdentity"
-		})
-@TypeHint(
-		types = { Unicorn.class, UnicornEventType.class}, access = { TypeAccess.PUBLIC_CONSTRUCTORS, TypeAccess.PUBLIC_METHODS }
-)
 public class StoreApplication {
 
 	public static void main(String[] args) {
