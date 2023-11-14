93445a00e530c170ff4f63f0932ca88a065f5904
Maximilian Schellhorn <mxschell@amazon.de>
2023-11-14 22:05:54 +0100

    Reset to initial baseline

--- labs/unicorn-store/infrastructure/cdk/src/main/java/com/unicorn/alternatives/UnicornAuditService.java
+++ labs/unicorn-store/infrastructure/cdk/src/main/java/com/unicorn/alternatives/UnicornAuditService.java
@@ -26,33 +26,6 @@ public class UnicornAuditService extends Stack {
         super(scope, id, props);
 
         //** Your code will go here **//
-        var auditTable = Table.Builder.create(this, "UnicornStoreAuditServiceTable")
-                .tableName("unicorn-audit")
-                .partitionKey(Attribute.builder().name("id").type(AttributeType.STRING).build())
-                .build();
-
-        //** Add your code for the AWS Lambda function creation below **/
-        var unicornAuditServiceFunction = Function.Builder.create(this, "UnicornAuditServiceFunction")
-                .runtime(Runtime.JAVA_17)
-                .functionName("unicorn-audit-service")
-                .memorySize(512)
-                .timeout(Duration.seconds(29))
-                .code(Code.fromAsset("../../software/alternatives/unicorn-audit-service/target/unicorn-audit-service-1.0.0-aws.jar"))
-                .handler("org.springframework.cloud.function.adapter.aws.FunctionInvoker::handleRequest")
-                .vpc(infrastructureStack.getVpc())
-                .securityGroups(List.of(infrastructureStack.getApplicationSecurityGroup()))
-                .build();
-
-        auditTable.grantWriteData(unicornAuditServiceFunction);
-
-        //** Trigger the Lambda Function from the existing Amazon EventBridge EventBus **/
-        var rule = Rule.Builder.create(this, "EventBridgeRuleAudit")
-                .ruleName("audit-service-rule")
-                .eventBus(infrastructureStack.getEventBridge())
-                .eventPattern(EventPattern.builder().source(List.of("com.unicorn.store")).build())
-                .build();
-
-        rule.addTarget(new LambdaFunction(unicornAuditServiceFunction));
     }
 
 }
