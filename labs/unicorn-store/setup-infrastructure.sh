fc3e86a9cb5a5e9d041d94db2cb960c45d2acc5b
Lefteris Karageorgiou <lefkarag@gmail.com>
2024-09-26 18:38:58 +0300

    wip

--- labs/unicorn-store/setup-infrastructure.sh
+++ labs/unicorn-store/setup-infrastructure.sh
@@ -21,13 +21,12 @@ cdk deploy UnicornStoreInfrastructure --require-approval never --outputs-file ta
 # Execute the DB Setup function to create the table
 lambda_result=$(aws lambda invoke --function-name $(cat target/output.json | jq -r '.UnicornStoreInfrastructure.DbSetupArn') /dev/stdout 2>&1)
 # Extract the status code from the response payload
-lambda_status_code=$(echo "$lambda_result" | jq -r '.statusCode')
+lambda_status_code=$(echo "$lambda_result" | jq 'first(.. | objects | select(has("statusCode"))) | .statusCode')
 
 if [ "$lambda_status_code" == "200" ]; then
-    echo "DB Setup function executed successfully."
+    echo "DB Setup Lambda function executed successfully"
 else
-    failed_reason="DB Setup function execution failed: $lambda_result"
-    echo $failed_reason
+    echo "DB Setup Lambda function execution failed"
     exit 1
 fi
 
