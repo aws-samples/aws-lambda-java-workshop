572c73dd8683a894c1b58cac5c912878893f0106
Dennis Kieselhorst <mail@dekies.de>
2024-11-20 15:40:43 +0100

    upgrade to PostgreSQL 16

--- labs/unicorn-store/infrastructure/cdk/src/main/java/com/unicorn/core/InfrastructureStack.java
+++ labs/unicorn-store/infrastructure/cdk/src/main/java/com/unicorn/core/InfrastructureStack.java
@@ -68,7 +68,7 @@ public class InfrastructureStack extends Stack {
     private DatabaseInstance createRDSPostgresInstance(IVpc vpc, DatabaseSecret databaseSecret) {
 
         var databaseSecurityGroup = createDatabaseSecurityGroup(vpc);
-        var engine = DatabaseInstanceEngine.postgres(PostgresInstanceEngineProps.builder().version(PostgresEngineVersion.VER_15).build());
+        var engine = DatabaseInstanceEngine.postgres(PostgresInstanceEngineProps.builder().version(PostgresEngineVersion.VER_16).build());
 
         return DatabaseInstance.Builder.create(this, "UnicornInstance")
                 .engine(engine)
